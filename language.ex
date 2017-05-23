f9d9596614afe34f7b3123bef9364e9d6156a4bd
kaism <eigenspaces@gmail.com>
2017-05-22 21:47:54 -0400

    .

---
+++ language.ex
@@ -0,0 +1,30 @@
+cell syntax
+
+function declaration
+    <int: a, char: b> <= func{char: c, char^: d} = (
+        |* code body *|
+    )
+
+cystil syntax
+
+function declaration
+    (-| * before variables means non-typed. -)
+
+    func * x y = [
+
+    ]
+
+    func int: x; char: y = [
+
+    ]
+
+
+echo "# data_structs" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/kaism/data_structs.git
+git push -u origin master
+
+git remote add origin https://github.com/kaism/data_structs.git
+git push -u origin master
